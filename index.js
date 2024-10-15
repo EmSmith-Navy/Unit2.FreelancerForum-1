1b33725659cbb3a6257b82f05b28e8f4d4e71fd8
EmSmith-Navy <keino2586@gmail.com>
2024-10-15 10:24:05 -0500

    update

---
+++ index.js
@@ -0,0 +1,10 @@
+const freelancers = [
+  { name: "Alice", price: 30, occupation: "writer" },
+  { name: "Bob", price: 50, occupation: "Teacher" },
+  { name: "Carol", price: 70, occupation: "teacher" },
+  { name: "David", price: 81, occupation: "teacher" },
+  { name: "Lydia", price: 43, occupation: "teacher" },
+  { name: "Darryl", price: 76, occupation: "programmer" },
+  { name: "Manuel", price: 47, occupation: "teacher" },
+  { name: "Olivia", price: 72, occupation: "driver" },
+];
